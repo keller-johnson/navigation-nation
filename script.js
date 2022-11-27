4c75c86d99232b265faaf987aac1d4f58529f647
keller-johnson <keller.s.johnson@gmail.com>
2022-11-26 19:51:02 -0800

    working full page sliding menu

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function basicOp(operation, value1, value2) {
-  let equation = `${value1} ${operation} ${value2}`;
-  return eval(equation);
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
+
+function toggleNav() {
+  //Toggle menu bars open and closed
+  menuBars.classList.toggle("change");
+  //Toggle menu active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    //Animate slide in
+    overlay.classList.remove("overlay-slide-left");
+    overlay.classList.add("overlay-slide-right");
+  } else {
+    //Animate slide out
+    overlay.classList.remove("overlay-slide-right");
+    overlay.classList.add("overlay-slide-left");
+  }
 }
 
-console.log(basicOp("/", 20, 2));
+// Eventlistener
+
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
